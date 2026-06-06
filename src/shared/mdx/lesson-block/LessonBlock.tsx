3cc19c3b1becf081b100274866cb14ca668db991
Bryan Grandon <bryan.grandon.c@gmail.com>
2026-06-05 22:41:45 -0400

    feat(web):add mdx.css and change related files

--- src/shared/mdx/lesson-block/LessonBlock.tsx
+++ src/shared/mdx/lesson-block/LessonBlock.tsx
@@ -11,13 +11,13 @@ const LessonBlock = ({ children, variant }: LessonBlockProps) => {
   const Icon = config.icon
   const title = config.title
   return (
-    <section className='rounded-xl border border-slate-200 bg-white p-4'>
+    <section className='rounded-xl border border-mdx-border bg-mdx-surface p-4'>
       <section className='flex items-center gap-2'>
-        <Icon size={18} />
-        <h3 className='font-semibold text-slate-900'>{title}</h3>
+        <Icon size={18} color='var(--mdx-level-color)' />
+        <p className='font-semibold text-lg text-mdx-text-primary'>{title}</p>
       </section>
 
-      <div className='my-4 border-t border-slate-100'></div>
+      <div className='my-3 border-t border-mdx-border'></div>
 
       {children}
     </section>
