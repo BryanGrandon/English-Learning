885d2503b9778653f044bda6f5342738a4cb8998
Bryan Grandon <bryan.grandon.c@gmail.com>
2026-06-08 18:24:34 -0400

    refactor(web):change index.astro and mdx file

--- src/shared/mdx/lesson-block/LessonBlock.tsx
+++ src/shared/mdx/lesson-block/LessonBlock.tsx
@@ -11,15 +11,14 @@ const LessonBlock = ({ children, variant }: LessonBlockProps) => {
   const Icon = config.icon
   const title = config.title
   return (
-    <section className='rounded-xl border border-mdx-border bg-mdx-surface p-4'>
+    <section className='rounded-xl border border-mdx-border bg-mdx-surface-secondary p-4'>
       <section className='flex items-center gap-2'>
-        <Icon size={18} color='var(--mdx-level-color)' />
+        <Icon size={18} color='var(--level-a1)' />
         <p className='font-semibold text-lg text-mdx-text-primary'>{title}</p>
       </section>
 
       <div className='my-3 border-t border-mdx-border'></div>
-
-      {children}
+      <section className='text-mdx-text-secondary ml-4'>{children}</section>
     </section>
   )
 }
