8a7010ff8e9e9e1f0dc1c8ee462779a1c32ca951
Bryan Grandon <bryan.grandon.c@gmail.com>
2026-06-10 18:16:18 -0400

    refactor(web):change mdx and slyles

--- src/shared/mdx/lesson-block/LessonBlock.tsx
+++ src/shared/mdx/lesson-block/LessonBlock.tsx
@@ -11,7 +11,7 @@ const LessonBlock = ({ children, variant }: LessonBlockProps) => {
   const Icon = config.icon
   const title = config.title
   return (
-    <section className='rounded-xl border border-mdx-border bg-mdx-surface-secondary p-4'>
+    <section className='rounded-xl border border-mdx-border bg-mdx-surface p-4'>
       <section className='flex items-center gap-2'>
         <Icon size={18} color='var(--level-a1)' />
         <p className='font-semibold text-lg text-mdx-text-primary'>{title}</p>
