8031cda553a608118bd8e42bda0e857552ccea06
Bryan Grandon <bryan.grandon.c@gmail.com>
2026-08-19 22:05:49 -0400

    refactor(web):change Componentes react to astro

--- src/shared/components/content/callout/Callout.tsx
+++ src/shared/components/content/callout/Callout.tsx
@@ -5,23 +5,26 @@ type CalloutProps = {
   type: CalloutVariant
   children: React.ReactNode
   className?: string
+  fullWidth?: boolean
   title?: string
 }
 
-const Callout = ({ type, children, className = '', title }: CalloutProps) => {
+const Callout = ({ type, children, className = '', title, fullWidth = false }: CalloutProps) => {
   const { color, background, label, icon } = CALLOUT_CONFIG[type]
   const Icon = icon
 
+  const widthContent = fullWidth ? 'w-full' : 'w-fit '
+
   return (
     <section
-      className={cn('flex flex-col gap-4 p-4 border rounded-lg', className)}
+      className={cn('flex flex-col gap-4 p-4 border rounded-lg w-fit', className, widthContent)}
       style={{ borderColor: color, backgroundColor: background }}
     >
       <header className='flex items-center gap-4 pb-2 border-b' style={{ borderColor: color }}>
         <div className='flex items-center justify-center size-8  rounded-lg ' style={{ backgroundColor: color }}>
           <Icon size={18} color='white' />
         </div>
-        <h3 className='font-bold capitalize text-xl'>{title ?? label}</h3>
+        <p className='capitalize text-lg'>{title ?? label}</p>
       </header>
       <section className='grid gap-4'>{children}</section>
     </section>
